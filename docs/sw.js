54779b6ff005954fa5944f4f1a8e48ceecc516b7
Roman Zolotarev <hi@romanzolotarev.com>
2022-06-04 22:40:20 +0300

    clean up pwa

--- docs/sw.js
+++ docs/sw.js
@@ -1,60 +1,56 @@
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1.0.11'
-const RUNTIME = 'runtime'
-
-// A list of local resources we always want to be cached.
+const PRECACHE = "2022060405";
+const RUNTIME = "runtime";
 const PRECACHE_URLS = [
-  './',
-  'index.html',
-  '0.min.css',
-  'rgzee-black.svg'
-]
+  "./",
+  "index.html",
+  "0.min.css",
+  "rgzee.svg",
+  "favicon.png",
+  "favicon-512.png",
+];
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  console.log('install')
+const _install = (event) =>
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(PRECACHE_URLS))
       .then(self.skipWaiting())
-  )
-})
+  );
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME]
+const _activate = (event) =>
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete)
-      }))
-    }).then(() => self.clients.claim())
-  )
-})
+    caches
+      .keys()
+      .then((cacheNames) =>
+        cacheNames.filter(
+          (cacheName) => ![PRECACHE, RUNTIME].includes(cacheName)
+        )
+      )
+      .then((cachesToDelete) =>
+        Promise.all(
+          cachesToDelete.map((cacheToDelete) => caches.delete(cacheToDelete))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse
-        }
+const isSameOrigin = (event) =>
+  event.request.url.startsWith(self.location.origin);
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => response)
-          })
-        })
-      })
-    )
-  }
-})
+const putToCache = (event) => (cache) =>
+  fetch(event.request).then((response) =>
+    cache.put(event.request, response.clone()).then(() => response)
+  );
+
+const _fetch = (event) =>
+  isSameOrigin(event) &&
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return caches.open(RUNTIME).then(putToCache(event));
+    })
+  );
+
+self.addEventListener("install", _install);
+self.addEventListener("activate", _activate);
+self.addEventListener("fetch", _fetch);
